7a71400ce4d47662641151c4a9104ce09d78a252
David Dume <dumedavid11@gmail.com>
2023-10-04 12:38:42 +0200

    creato le crud

--- la-mia-pizzeria-crud-mvc/Models/Pizza.cs
+++ la-mia-pizzeria-crud-mvc/Models/Pizza.cs
@@ -1,10 +1,20 @@
-﻿namespace la_mia_pizzeria_crud_mvc.Models
+﻿using la_mia_pizzeria_crud_mvc.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace la_mia_pizzeria_crud_mvc.Models
 {
     public class Pizza
     {
-        public int Id { get; set; }
+        [Key] public int Id { get; set; }
+        [Required(ErrorMessage = "il nome è obbligatorio")]
+        [MaxLength(100, ErrorMessage = "La lunghezza massima è di 100 caratteri")]
         public string Name { get; set; }
+        [Pizza5Words]
+        [Required(ErrorMessage = "La descrizione è obbligatoria")] 
+        [Column(TypeName = "text")] 
         public string Description { get; set; }
+        [Url(ErrorMessage = "Devi inserire un link valido")]
+        [MaxLength(500, ErrorMessage = "Il link non può essere più lungo di 500 caratteri")]
         public string Image { get; set; }
         public float Price { get; set; }
 
